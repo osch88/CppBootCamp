b1b2608fa2349f642483aab54e8d4677540bc6db
osch88 <kristiansson.oscar@gmail.com>
2021-10-09 10:15:37 +0200

    parser OK

--- mini-project/main.cpp
+++ mini-project/main.cpp
@@ -9,18 +9,20 @@ const unsigned int SIZE = 9;
 
 typedef struct SudokoCell
 {
-    int             value;
+    char            value;
     bool            possibleSolutions[SIZE];
 }SudokoCell_t;
 
 
-bool parser(const std::string &fileName, SudokoCell_t SudokoTable[][SIZE]){
+void parser(const std::string &fileName, SudokoCell_t SudokoTable[][SIZE]){
     /* 
         - Read in a csv file to a string
         - Check: Must be exact 81 digits
-        - Return string
+        - Parse the csv file into an 2D array
+        - By creating a new instance of struct SudokoCell
+        - Update SudokoCell->value
     */
-    bool success = false;
+    // bool success = false;
     std::ifstream file(fileName);
 
     if( file.is_open() ){
@@ -31,43 +33,28 @@ bool parser(const std::string &fileName, SudokoCell_t SudokoTable[][SIZE]){
         {
             if( isdigit(input) ){ numbers.push_back(input); }
         }
-
-        for (size_t i = 0; i < numbers.size(); i++)
+        // Need a check if size == 81
+        size_t row = 0; size_t col = 0;
+        for (size_t k = 0; k < numbers.size(); k++)
         {
-            std::cout << numbers[i] << " ";
-            if( (i+1) % 9 == 0){ std::cout <<"\n"; }
+            SudokoTable[row][col].value = numbers[k];
+            col++;
+            if( !(k+1)%9 ){ // Takes care of new a row in the table
+                row++;
+                col = 0;
+            }
         }
-        std::cout << "\n" << numbers.size() << std::endl;
 
         file.close();
-        success = true;
+        // success = true;
     }
     else{
         std::cout << "nothing..\n";
-        success = false;
+        // success = false;
     };
 
-    return success;
+    // return success;
 
-};
-
-
-void parser(std::ifstream *pInputSudoko, SudokoCell_t SudokoTable[][SIZE]){
-    /*
-        - Parse the csv file into an 2D array
-        - By creating a new instance of struct SudokoCell
-        - Update SudokoCell->value
-    */
-   int val;
-   for (size_t i = 0; i < SIZE; i++)
-   {
-       SudokoTable[i][SIZE];
-       for (size_t j = 0; j < SIZE; j++)
-       {
-           SudokoTable[i][j].value = val;
-       }
-       
-   }
 };
 
 
@@ -93,21 +80,10 @@ void printer(SudokoCell_t SudokoTable[][SIZE]){
        {
            std::cout << SudokoTable[i][j].value << " ";
        }
+       std::cout << "\n";
    }
 };
 
-
-int Arr[SIZE][SIZE] = {
-    {5,3,0,0,7,0,0,0,0},
-    {6,0,0,1,9,5,0,0,0},
-    {0,9,8,0,0,0,0,6,0},
-    {8,0,0,0,6,0,0,0,3},
-    {4,0,0,8,0,3,0,0,1},
-    {7,0,0,0,2,0,0,0,6},
-    {0,6,0,0,0,0,2,8,0},
-    {0,0,0,4,1,9,0,0,5},
-    {0,0,0,0,8,0,0,7,9}
-};
 
 int main(int argc, char *argv[]){
 
@@ -117,11 +93,8 @@ int main(int argc, char *argv[]){
 
 
     parser(fileName, SudokoTable);
+    printer(SudokoTable);
 
-    // std::cout << pInputSudoko << std::endl;
-
-    // parser(pInputSudoko, SudokoTable);
-    // printer(SudokoTable);
 
     return 0;
 }
