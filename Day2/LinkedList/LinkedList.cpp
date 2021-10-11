339b46d5a62e1607926c2af13514ebddbd51f87a
osch88 <kristiansson.oscar@gmail.com>
2021-10-11 10:19:33 +0200

    smaller changes

--- Day2/LinkedList/LinkedList.cpp
+++ Day2/LinkedList/LinkedList.cpp
@@ -1,20 +1,32 @@
+/*
+    Implement a single (forward) linked list for integers, your application should be able to:
+    - Add items to the end of the list
+    - Remove items from the front the list
+    - Print the list in the correct order
+*/
+
 #include <iostream>
+
+
 typedef struct Node {
-    int     data;
-    struct Node *next;    
+    int             data;
+    struct Node     *next;    
 }Node_t;
+
 
 Node_t *findLast(Node_t *LinkedList) {
     if (LinkedList == nullptr) {    
         return LinkedList;
     }
     Node_t *current = LinkedList;
-     while (current->next != nullptr) {
+    while (current->next != nullptr) {
         current = current->next;
     }
     return current;
 }
-void add(Node_t *LinkedList, int value) {
+
+
+void add(Node_t *LinkedList, const int &value) {
     // 1) find the last item
     // 2)create a new node
     // 3) assign & link
@@ -30,36 +42,61 @@ void add(Node_t *LinkedList, int value) {
         lastNode->next = newNode;    
     }
 }
-Node_t *remove(Node_t *LinkedList) {
+
+
+Node_t *removeFromHead(Node_t *LinkedList) {
     if (LinkedList == nullptr) {
         return LinkedList;
     }
-    // 1) find the head of next and set is as new head
-    // 2) delete head
+    /*
+        1) find the head of next and set is as new head
+        2) delete head
+    */
     Node_t *next = LinkedList->next;
     delete LinkedList;
     return next;
 }
+
+
 void print(Node_t *LinkedList) {
-    // 1)loop over the list
-    // 2) print things
-    if (LinkedList == nullptr) return;
+    /*  
+        1) loop over the list
+        2) print things
+    */
+    // if (LinkedList == nullptr) return;
     while(LinkedList != nullptr) {
         std::cout << LinkedList->data << std::endl; 
         LinkedList = LinkedList->next;       
     }
+    std::cout << "Something\n";
 }
+
+void deleteList(Node_t *LinkedList){
+    Node_t *temp = LinkedList;
+    Node_t *temp2;
+
+    while ( temp != nullptr )
+    {
+        temp2 = LinkedList->next;
+        delete temp;
+        temp = temp2;
+    }
+    
+}
+
+
 int main (int argc, char ** argv) {
-    Node_t * MY_LINKED_LIST = nullptr;
-    add (MY_LINKED_LIST, 12);
-    add (MY_LINKED_LIST, 152);
-    add (MY_LINKED_LIST, 1232);
-    add (MY_LINKED_LIST, 121);
-    add (MY_LINKED_LIST, 182);
-    add (MY_LINKED_LIST, 132);
-    add (MY_LINKED_LIST, 127);
-    add (MY_LINKED_LIST, 1211);
-    add (MY_LINKED_LIST, 1233);
+
+    Node_t *MY_LINKED_LIST = nullptr;
+    // Not enough. Must create a intance new.
+    
+    
+    add(MY_LINKED_LIST, 12);
+    // MY_LINKED_LIST->data = 5;
+
+    print(MY_LINKED_LIST);
+
+
     
     return 0;
 }
--- Day2/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(int argc,char* argv[]) {
-    std::cout << "Proper Hello World, from: " << argv[1] << std::endl;
-    return 0;
-}
